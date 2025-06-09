ace90d9180b412163bc12a40add7951c0be825ed
casolaroSalvatore <150945794+casolaroSalvatore@users.noreply.github.com>
2025-06-09 21:48:01 +0200

    Delete app directory

--- app/src/main/java/com/example/mastermind/domain/model/ColorPeg.kt
+++
@@ -1,9 +0,0 @@
-package com.example.mastermind.domain.model
-
-enum class ColorPeg {
-    RED, GREEN, BLUE, YELLOW, ORANGE, PURPLE, CYAN, MAGENTA, BROWN, BLACK;
-
-    companion object {
-        fun subset(count: Int) = values().take(count)
-    }
-}
--- app/src/main/java/com/example/mastermind/domain/model/GameSettings.kt
+++
@@ -1,7 +0,0 @@
-package com.example.mastermind.domain.model
-
-data class GameSettings(
-    val colors: Int = 6,          // 6, 8, 10
-    val codeLength: Int = 4,      // 4 o 5
-    val allowDuplicates: Boolean = false
-)
